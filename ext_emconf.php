5e388a6b44ad433d310704a9f7484e6a2cadde3f
Daniel Lienert <daniel@lienert.cc>
2011-03-13 14:33:29 +0100

    CHG: uploaded to TER -> v 0.1.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "yag_theme_simpleviewer".
 #
-# Auto generated 07-03-2011 18:29
+# Auto generated 13-03-2011 14:15
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -18,7 +18,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_email' => 'daniel@lienert.cc',
 	'author_company' => '',
 	'shy' => '',
-	'dependencies' => 'cms,extbase,fluid',
+	'dependencies' => 'cms,extbase,fluid,yag',
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
@@ -29,12 +29,13 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '0.1.1',
+	'version' => '0.1.2',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
 			'extbase' => '',
 			'fluid' => '',
+			'yag' => '1.0.5',
 		),
 		'conflicts' => array(
 		),
@@ -43,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:23:{s:12:"ext_icon.gif";s:4:"e6c8";s:17:"ext_localconf.php";s:4:"c867";s:14:"ext_tables.php";s:4:"8854";s:14:"ext_tables.sql";s:4:"d41d";s:16:"kickstarter.json";s:4:"c1b6";s:34:"Configuration/TypoScript/setup.txt";s:4:"aa4f";s:55:"Configuration/TypoScript/Themes/SimpleViewer/General.ts";s:4:"d7e9";s:56:"Configuration/TypoScript/Themes/SimpleViewer/ItemList.ts";s:4:"03ea";s:53:"Configuration/TypoScript/Themes/SimpleViewer/Theme.ts";s:4:"f221";s:40:"Resources/Private/Language/locallang.xml";s:4:"ef62";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"046a";s:46:"Resources/Private/Templates/ItemList/List.html";s:4:"2e04";s:49:"Resources/Private/Templates/ItemList/XmlList.html";s:4:"3cb1";s:39:"Resources/Public/SimpleViewer/full.html";s:4:"1feb";s:50:"Resources/Public/SimpleViewer/css/simpleviewer.css";s:4:"2959";s:44:"Resources/Public/SimpleViewer/img/assets.png";s:4:"8b9e";s:48:"Resources/Public/SimpleViewer/img/caption-bg.png";s:4:"fb78";s:49:"Resources/Public/SimpleViewer/img/empty-pixel.png";s:4:"35a1";s:45:"Resources/Public/SimpleViewer/img/spinner.gif";s:4:"76ce";s:52:"Resources/Public/SimpleViewer/js/jquery-1.4.4.min.js";s:4:"73a9";s:48:"Resources/Public/SimpleViewer/js/simpleviewer.js";s:4:"88da";s:45:"Resources/Public/SimpleViewer/js/swfobject.js";s:4:"892a";s:50:"Resources/Public/SimpleViewer/swf/simpleviewer.swf";s:4:"fc38";}',
+	'_md5_values_when_last_written' => 'a:23:{s:12:"ext_icon.gif";s:4:"e6c8";s:17:"ext_localconf.php";s:4:"c867";s:14:"ext_tables.php";s:4:"8854";s:14:"ext_tables.sql";s:4:"d41d";s:16:"kickstarter.json";s:4:"c1b6";s:34:"Configuration/TypoScript/setup.txt";s:4:"aa4f";s:55:"Configuration/TypoScript/Themes/SimpleViewer/General.ts";s:4:"d7e9";s:56:"Configuration/TypoScript/Themes/SimpleViewer/ItemList.ts";s:4:"03ea";s:53:"Configuration/TypoScript/Themes/SimpleViewer/Theme.ts";s:4:"f221";s:40:"Resources/Private/Language/locallang.xml";s:4:"ef62";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"046a";s:46:"Resources/Private/Templates/ItemList/List.html";s:4:"ba02";s:49:"Resources/Private/Templates/ItemList/XmlList.html";s:4:"eba6";s:39:"Resources/Public/SimpleViewer/full.html";s:4:"1feb";s:50:"Resources/Public/SimpleViewer/css/simpleviewer.css";s:4:"2959";s:44:"Resources/Public/SimpleViewer/img/assets.png";s:4:"8b9e";s:48:"Resources/Public/SimpleViewer/img/caption-bg.png";s:4:"fb78";s:49:"Resources/Public/SimpleViewer/img/empty-pixel.png";s:4:"35a1";s:45:"Resources/Public/SimpleViewer/img/spinner.gif";s:4:"76ce";s:52:"Resources/Public/SimpleViewer/js/jquery-1.4.4.min.js";s:4:"73a9";s:48:"Resources/Public/SimpleViewer/js/simpleviewer.js";s:4:"88da";s:45:"Resources/Public/SimpleViewer/js/swfobject.js";s:4:"892a";s:50:"Resources/Public/SimpleViewer/swf/simpleviewer.swf";s:4:"fc38";}',
 );
 
 ?>
